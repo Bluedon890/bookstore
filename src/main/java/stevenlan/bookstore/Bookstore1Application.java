0986a3fadcd47cf52d5908c2b6bdd07515bd9fad
Bluedon890 <10255026ekids178@gmail.com>
2024-06-24 17:58:32 +0800

    test

--- src/main/java/stevenlan/bookstore/Bookstore1Application.java
+++ src/main/java/stevenlan/bookstore/Bookstore1Application.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+//test
 public class Bookstore1Application {
 
 	public static void main(String[] args) {
